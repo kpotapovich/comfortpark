4b2a56d185fd5290148a0ef419dfb99edfb8923c
kpotapovich <kris.potapovich@gmail.com>
2020-12-23 19:47:18 +0300

    доделала оставшиеся container, дофавила img

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ window.onscroll = function () {
  }else {
      header_panel.style.background = "#009999";
      header_panel.style.boxShadow = "0px  2px 5px #008080";
-     header_panel.style.opacity = 0.7;
+     header_panel.style.opacity = 0.8;
      // header_panel.style.background = "transparent";
      // header_panel.style.boxShadow = "none";
  }
 }
+
+// window.onresize = function() {
+//     const width = window.outerWidth;
+//     let burger = document.getElementById('burger');
+//     let header_panel = document.getElementById('header_panel');
+//     let menu_panel = document.getElementById('header_menu');
+//
+//     if (width > 1200) {
+//         burger.style.display = "none";
+//         header_panel.appendChild(menu_panel);
+//     } else if (width > 800 && width < 1200) {
+//         burger.style.display = "none";
+//         header_panel.appendChild(menu_panel);
+//     } else if (width < 800) {
+//         burger.style.display = "block";
+//         burger.appendChild(menu_panel);
+//     }
+// }
+
+// let burger = document.getElementById('burger');
+// window.on
+
+// document.querySelector('.burger').addEventListener('click', function(){
+//     document.querySelector('.burger span').classList.toggle('active');
+// })
