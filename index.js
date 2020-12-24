4dbbb339b0cc891e1860c70b6561ed4aa6545449
Andrey Narut <andrey.narut.90@gmail.com>
2020-12-24 03:24:51 +0300

    fix burger

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let header_panel = document.getElementById('header_panel');
-window.onscroll = function () {
+const onScrollFunction = function () {
+    let header_panel = document.getElementById('header_panel');
     if (window.pageYOffset > 100) {
         header_panel.style.background = "#009999";
         header_panel.style.boxShadow = "0px  2px 5px #008080";
@@ -8,23 +8,14 @@ window.onscroll = function () {
         header_panel.style.background = "#009999";
         header_panel.style.boxShadow = "0px  2px 5px #008080";
         header_panel.style.opacity = 0.8;
-        // header_panel.style.background = "transparent";
-        // header_panel.style.boxShadow = "none";
     }
 }
 
-document.querySelector('.burger').addEventListener('click', function () {
-    document.querySelector('.burger span').classList.toggle('active');
-    document.querySelector('.menu_container').classList.toggle("animate");
-})
-
 const menuBuilder = function () {
-    console.log(window.outerWidth)
     const width = window.outerWidth;
     let burger_container = document.getElementById('burger_container');
     let menu_container = document.getElementById('menu_container');
     let menu_ul = document.getElementById('menu_ul');
-
 
     if (width > 980) {
         burger_container.style.display = "none"
@@ -35,9 +26,16 @@ const menuBuilder = function () {
         burger_container.style.display = "block"
         menu_container.style.top = null;
         menu_ul.style.display = "block"
-
     }
 }
 
 menuBuilder()
+onScrollFunction()
+
+window.onscroll = onScrollFunction;
 window.onresize = menuBuilder;
+
+document.querySelector('.burger').addEventListener('click', function () {
+    document.querySelector('.burger span').classList.toggle('active');
+    document.querySelector('.menu_container').classList.toggle("animate");
+})
