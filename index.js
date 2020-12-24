c80e23771083c48a96c4deebc40c6ceffcb2d278
kpotapovich <kris.potapovich@gmail.com>
2020-12-24 03:03:47 +0300

    code reformat

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 let header_panel = document.getElementById('header_panel');
 window.onscroll = function () {
- if (window.pageYOffset>100){
-     header_panel.style.background = "#009999";
-     header_panel.style.boxShadow = "0px  2px 5px #008080";
-     header_panel.style.opacity = 1;
- }else {
-     header_panel.style.background = "#009999";
-     header_panel.style.boxShadow = "0px  2px 5px #008080";
-     header_panel.style.opacity = 0.8;
-     // header_panel.style.background = "transparent";
-     // header_panel.style.boxShadow = "none";
- }
+    if (window.pageYOffset > 100) {
+        header_panel.style.background = "#009999";
+        header_panel.style.boxShadow = "0px  2px 5px #008080";
+        header_panel.style.opacity = 1;
+    } else {
+        header_panel.style.background = "#009999";
+        header_panel.style.boxShadow = "0px  2px 5px #008080";
+        header_panel.style.opacity = 0.8;
+        // header_panel.style.background = "transparent";
+        // header_panel.style.boxShadow = "none";
+    }
 }
 
-document.querySelector('.burger').addEventListener('click', function(){
-document.querySelector('.burger span').classList.toggle('active');
-document.querySelector('.menu_container').classList.toggle("animate");
+document.querySelector('.burger').addEventListener('click', function () {
+    document.querySelector('.burger span').classList.toggle('active');
+    document.querySelector('.menu_container').classList.toggle("animate");
 })
 
-const menuBuilder = function() {
+const menuBuilder = function () {
     console.log(window.outerWidth)
     const width = window.outerWidth;
     let burger_container = document.getElementById('burger_container');
